b21c28f47081e2e441cdd53cd2c6557faff890e6
ogomb <mbogolewis@gmail.com>
2018-07-02 19:41:59 +0300

    add sign up and login functionality to the application.

--- app.rb
+++ app.rb
@@ -7,6 +7,7 @@ class MyApp < Sinatra::Application
     set :environment, ENV['RACK_ENV']
 
     configure do
+        enable :sessions
         env = ENV['RACK_ENV']
         DB = Sequel.connect(YAML.load(File.open('database.yml'))[env])
         Dir[File.join(File.dirname(__FILE__), 'db', '*.rb')].each { |model| require model }
@@ -15,13 +16,47 @@ end
 
 
 get '/' do
+  @session = session[:user_id]
   erb :home
 end
 
-get '/signup' do
-  erb :sign_up
+get '/signup/?' do
+  if session[:user_id].nil?
+    erb :sign_up
+  else
+    @error = 'Please log out first'
+    erb :error
+  end
 end
 
-get '/login' do
-  erb :login
+post '/signup/?' do
+  user = User.new(name: params[:name], email: params[:email])
+  user.password = user.hash_password(params[:password])
+  user.save
+  redirect '/login'
+end
+
+get '/login/?' do
+  if session[:user_id].nil?
+    erb :login
+  else
+    @error = 'Please log out first'
+    erb :error
+  end
+end
+
+post '/login/?' do
+  user = User.first(email: params[:email])
+  if user.test_password(params[:password], user.password )
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    @error = 'Invalid login credentials'
+    erb :error
+  end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect '/login'
 end
--- db/user.rb
+++ db/user.rb
@@ -1,5 +1,14 @@
 require 'sequel'
+require 'bcrypt'
 
 class User < Sequel::Model
   set_primary_key :id
+
+  def hash_password(password)
+    BCrypt::Password.create(password).to_s
+  end
+
+  def test_password(password, hash)
+    BCrypt::Password.new(hash) == password
+  end
 end
