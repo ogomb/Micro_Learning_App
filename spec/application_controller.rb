3246b547f40d841fb5f24cea5b8f99bc601a079f
ogomb <mbogolewis@gmail.com>
2018-07-11 17:04:43 +0300

    tests for the application login and signup.

---
+++ spec/application_controller.rb
@@ -0,0 +1,43 @@
+require 'rack/test'
+require 'rspec'
+require_relative '../app/controllers/application_controller'
+
+module MicroLearningApp
+  RSpec.describe 'Micro Learning App' do
+    include Rack::Test::Methods
+
+    def app
+      ApplicationController.new
+    end
+
+    it 'should show sign-up form' do
+      get '/signup'
+      expect(last_response.status).to eq(200)
+    end
+    it 'should show login form and redirect to home' do
+      get '/login'
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'should redirect to login after signup' do
+      post '/signup', {name: 'esad',password: 'ereqreq', email: 'mbogolew@gmail.com',confirm_password: 'ereqreq'}
+      expect(last_response).to be_redirect
+      follow_redirect!
+      expect(last_request.path).to eq('/login')
+    end
+
+    it 'should redirect to home after login' do
+      post '/login', {password: 'ereqreq', email: 'mbogolew@gmail.com'}
+      expect(last_response).to be_redirect
+      follow_redirect!
+      expect(last_request.path).to eq('/home')
+    end
+
+    it 'should redirect to logout' do
+      get '/logout'
+      expect(last_response).to be_redirect
+      follow_redirect!
+      expect(last_request.path).to eq('/login')
+    end
+  end
+end
